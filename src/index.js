e3676109f64cb642e54c005ede061cfb7553e3a7
Michał Ciesielski <majalcmaj@gmail.com>
2017-09-06 16:33:18 +0200

    Finished with css styling

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyBFN-yvxMDuDu9GkBuro-WWnjl1HkGU8-s';
 
@@ -10,17 +12,28 @@ class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { videos: [] };
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
 
-        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-            this.setState({videos})
+        YTSearch({key: API_KEY, term: 'backroll kite'}, (videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            })
         });
     }
+
 
     render() {
         return (
             <div>
-                <SearchBar/>
+                <SearchBar />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos} />
             </div>
         );
     }
