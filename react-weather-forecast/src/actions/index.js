7cb04ae875dba1ce18e2d1dc04682c846c767d29
Michał Ciesielski <majalcmaj@gmail.com>
2017-09-08 17:32:42 +0200

    WeatherForecast - further work

---
+++ react-weather-forecast/src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = 'adb0189d9278086a4ff3e5adea83d932';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?APPID=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
