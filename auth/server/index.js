42e7c0524e7da0ecf2caf606dc0044ce6234fc77
Michał Ciesielski <michal.ciesielski@softrender.pl>
2017-10-18 13:07:19 +0200

    further work on client

--- auth/server/index.js
+++ auth/server/index.js
@@ -3,6 +3,8 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 const router = require('./router');
 
 const app = express();
@@ -12,6 +14,7 @@ mongoose.connect('mongodb://localhost/auth', {useMongoClient: true});
 
 // App setup
 app.use(morgan('combined'));
+app.use(cors());
 app.use(bodyParser.json({type: '*/*'}));
 router(app);
 
--- auth/server/router.js
+++ auth/server/router.js
@@ -7,7 +7,7 @@ const requireSigning = passport.authenticate('local', {session: false});
 
 module.exports = function(app) {
     app.get('/', requireAuth, function (req, res) {
-       res.send({hi: 'there'})
+       res.send({message: "Super secret code is ABC123"})
     });
 
     app.post('/signin', requireSigning, Authentication.signin);
